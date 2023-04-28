a6fbf6e8e393612faa853c0870709d485261b839
gokulvasanthJ <122958037+gokulvasanthJ@users.noreply.github.com>
2023-04-28 20:23:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
+//div element creating in js programatically.
+
 var container=document.createElement("div");
 container.className="container";
+
+//div element creating in js.
 
 var row=document.createElement("div");
 row.classList.add("column");
 
+//append the row into the container.
+
 container.append(row);
+
+//fetch the book api datas.
 
 var res=fetch("https://anapioficeandfire.com/Api/books")
 res.then((data)=>data.json()).then((data1)=>foo(data1));
 
 function foo(data1){
     for(var i=0; i<data1.length; i++){
+        
+//creating html elements in js using literal.
+        
         row.innerHTML+=`
         <div class="containerall">
         <div class="card" >
         <h5 class="card-title">${data1[i].name}</h5>
-        <div class="imgdiv">
-        
-  
+        <div class="imgdiv"> 
   <ul class="list-group list-group-flush">
     <li class="list-group-item"><span style=color:#2dffc4;>Isbn:</span> <span style=color:#c976ba;> ${data1[i].isbn}</span></li>
     <li class="list-group-item"><span style=color:#2dffc4;>Authors:</span> <span style=color:#c976ba;>${data1[i].authors}</span></li>
@@ -33,6 +42,8 @@ function foo(data1){
 </div>
 
       `;
+        
+//append the container value in to the body of document.
       document.body.append(container);
     }
 }
